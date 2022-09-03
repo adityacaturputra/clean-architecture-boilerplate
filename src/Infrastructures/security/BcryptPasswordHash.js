f675f8b291c3c499da1c6400572be9de6fea4ae8
adityacaturputra <adityacaturputra25@gmail.com>
2022-09-03 18:01:45 +0700

    change password hash variable naming

--- src/Infrastructures/security/BcryptPasswordHash.js
+++ src/Infrastructures/security/BcryptPasswordHash.js
@@ -1,7 +1,7 @@
-const EncryptionHelper = require('../../Applications/security/PasswordHash');
+const PasswordHash = require('../../Applications/security/PasswordHash');
 const AuthenticationError = require('../../Commons/exceptions/AuthenticationError');
 
-class BcryptPasswordHash extends EncryptionHelper {
+class BcryptPasswordHash extends PasswordHash {
   constructor(bcrypt, saltRound = 10) {
     super();
     this._bcrypt = bcrypt;
--- src/Infrastructures/security/_test/BcryptPasswordHash.test.js
+++ src/Infrastructures/security/_test/BcryptPasswordHash.test.js
@@ -1,43 +1,43 @@
 const bcrypt = require('bcrypt');
 const AuthenticationError = require('../../../Commons/exceptions/AuthenticationError');
-const BcryptEncryptionHelper = require('../BcryptPasswordHash');
+const BcryptPasswordHash = require('../BcryptPasswordHash');
 
-describe('BcryptEncryptionHelper', () => {
+describe('BcryptPasswordHash', () => {
   describe('hash function', () => {
     it('should encrypt password correctly', async () => {
       // Arrange
       const spyHash = jest.spyOn(bcrypt, 'hash');
-      const bcryptEncryptionHelper = new BcryptEncryptionHelper(bcrypt);
+      const bcryptPasswordHash = new BcryptPasswordHash(bcrypt);
 
       // Action
-      const encryptedPassword = await bcryptEncryptionHelper.hash('plain_password');
+      const encryptedPassword = await bcryptPasswordHash.hash('plain_password');
 
       // Assert
       expect(typeof encryptedPassword).toEqual('string');
       expect(encryptedPassword).not.toEqual('plain_password');
-      expect(spyHash).toBeCalledWith('plain_password', 10); // 10 adalah nilai saltRound default untuk BcryptEncryptionHelper
+      expect(spyHash).toBeCalledWith('plain_password', 10); // 10 adalah nilai saltRound default untuk BcryptPasswordHash
     });
   });
 
   describe('comparePassword function', () => {
     it('should throw AuthenticationError if password not match', async () => {
       // Arrange
-      const bcryptEncryptionHelper = new BcryptEncryptionHelper(bcrypt);
+      const bcryptPasswordHash = new BcryptPasswordHash(bcrypt);
 
       // Act & Assert
-      await expect(bcryptEncryptionHelper.comparePassword('plain_password', 'encrypted_password'))
+      await expect(bcryptPasswordHash.comparePassword('plain_password', 'encrypted_password'))
         .rejects
         .toThrow(AuthenticationError);
     });
 
     it('should not return AuthenticationError if password match', async () => {
       // Arrange
-      const bcryptEncryptionHelper = new BcryptEncryptionHelper(bcrypt);
+      const bcryptPasswordHash = new BcryptPasswordHash(bcrypt);
       const plainPassword = 'secret';
-      const encryptedPassword = await bcryptEncryptionHelper.hash(plainPassword);
+      const encryptedPassword = await bcryptPasswordHash.hash(plainPassword);
 
       // Act & Assert
-      await expect(bcryptEncryptionHelper.comparePassword(plainPassword, encryptedPassword))
+      await expect(bcryptPasswordHash.comparePassword(plainPassword, encryptedPassword))
         .resolves.not.toThrow(AuthenticationError);
     });
   });
